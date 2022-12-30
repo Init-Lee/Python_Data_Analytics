33efa81885869f70897ae3c4e2ec251d31f56e85
Init-Lee <53702392+Init-Lee@users.noreply.github.com>
2022-12-30 16:42:23 +0800

    Update Font_Style.py
    增加系统判断

--- Font_Style.py
+++ Font_Style.py
@@ -1,7 +1,13 @@
 #写入魔法命令，设定图表样式
 
 import matplotlib.pyplot as plt
-#plt.rcParams['font.sans-serif'] = ['SimHei'] #windows os下支持中文，用于正常显示中文标签
-plt.rcParams['font.sans-serif'] = ['Arial Unicode MS'] #mac os 下显示中文 二选一 两行代码
+import platform
+    
+sys = platform.system().lower()#获取系统类型
+if(sys == 'win32'): #判定系统类型
+  plt.rcParams['font.sans-serif'] = ['SimHei'] #windows os下支持中文，用于正常显示中文标签
+else:
+  plt.rcParams['font.sans-serif'] = ['Arial Unicode MS'] #mac os 下显示中文 二选一 两行代码
+#end if
 plt.rcParams['font.size'] = 18 #调整字体大小
 #以上-------字体格式代码，根据运行环境自行调整------------------------------
